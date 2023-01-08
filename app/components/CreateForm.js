8ce386e56bc652e73fa7c10d9d444443eae39ba1
osamadado123 <osama.aldado@gmail.com>
2023-01-08 01:25:20 +0000

    done

--- app/components/CreateForm.js
+++ app/components/CreateForm.js
@@ -49,59 +49,59 @@ export default function CreateForm(props) {
       <section className=" flex justify-center ">
         <form
           onSubmit={handleSubmit}
-          className="relative flex flex-col break-words mb-6 rounded-lg bg-gray-400 rounded-3xl shadow-2xl w-4/5"
+          className="relative flex flex-col break-words mb-6 rounded-lg bg-gray-300 dark:bg-gray-500 rounded-3xl shadow-2xl w-4/5"
         >
-          <div className="mt-7 mb-10 text-center border-b-2 ">
-            <span className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-300 bg-gray-300 py-4 px-5 shadow-lg">
+          <div className="mt-7 mb-10 text-center border-b-2 dark:border-black ">
+            <span className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-200 dark:bg-gray-400  py-4 px-5 shadow-lg">
               Create Cookie Stand
             </span>
           </div>
           <div className=" flex justify-center">
-            <label className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
+            <label className=" text-xl  text-gray-800 dark:text-gray-200 rounded-3xl border-1 border-gray-400 bg-gray-300 dark:bg-gray-700 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
               Location
             </label>
             <input
               type="text"
               name="location"
               onChange={handleLocation}
-              className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
+              className=" text-xl  text-gray-800 dark:text-gray-100 rounded-3xl border-1 border-gray-100 dark:bg-gray-40 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
             ></input>
           </div>
           <div className=" flex justify-center mt-1">
-            <label className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
+            <label className=" text-xl  text-gray-800 dark:text-gray-200 rounded-3xl border-1 border-gray-400 bg-gray-300 dark:bg-gray-700 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
               Minimum Customers per Hour
             </label>
             <input
               type="text"
               name="minCustomers"
               onChange={handleMinCustomers}
-              className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
+              className=" text-xl dark:text-gray-100 text-gray-800 rounded-3xl border-1 border-gray-100 dark:bg-gray-400 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
             ></input>
           </div>
           <div className=" flex justify-center mt-1">
-            <label className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
+            <label className=" text-xl  text-gray-800 dark:text-gray-200 rounded-3xl border-1 border-gray-400 bg-gray-300 dark:bg-gray-700 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
               Maximum Customers per Hour
             </label>
             <input
               type="text"
               name="maxCustomers"
               onChange={handleMaxCustomers}
-              className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
+              className=" text-xl dark:text-gray-100  text-gray-800 rounded-3xl border-1 border-gray-100 dark:bg-gray-400 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
             ></input>
           </div>
           <div className=" flex justify-center mt-1">
-            <label className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
+            <label className=" text-xl  text-gray-800 dark:text-gray-200 rounded-3xl border-1 border-gray-400 bg-gray-300 dark:bg-gray-700 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
               Average Cookies per Sale
             </label>
             <input
               type="text"
               name="avgCookies"
               onChange={handleAvgCookies}
-              className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
+              className=" text-xl dark:text-gray-100 text-gray-800 rounded-3xl border-1 border-gray-100 dark:bg-gray-400 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
             ></input>
           </div>
           <div className="my-4 text-right mr-4 ">
-            <button className=" text-xl  text-gray-200 rounded-3xl border-1 border-indigo-200 bg-sky-700 py-4 px-5 shadow-lg hover:bg-sky-500">
+          <button className=" text-xl  text-gray-700 rounded-3xl border-1 border-indigo-200 bg-sky-400 dark:text-gray-100 dark:hover:bg-sky-400 dark:bg-sky-700 py-4 px-5 shadow-lg hover:bg-sky-200 hover:text-gray-600">
               Create
             </button>
           </div>
--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer(){
     return (
-        <footer  className="py-5 bg-gray-700 text-center text-white">
+        <footer  className="py-5 bg-gray-600  dark:bg-gray-800 text-center text-white">
            Ⓒ All Rights reserved 2023 
         </footer>
     )
